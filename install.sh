d7c0c70e1a3eab493d60419c631dcf79433e7fbe
Sergey Manzhos <opghld@gmail.com>
2023-03-23 22:58:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,3 +18,11 @@ screen -S fleek -X stuff "cd $HOME/ursa\n"
 screen -S fleek -X stuff "./ursa\n"
 curl https://ipfs.io/ipfs/bafybeidqdywrzg7c3b4dmm332m4b7uiakgitplz2pep2zntederxpj3odi -o basic.car
 ursa rpc put basic.car
+Теперь попробуем получить наш файл из сети
+ursa rpc get bafybeifyjj2bjhtxmp235vlfeeiy7sz6rzyx3lervfk3ap2nyn4rggqgei ./output
+При успешном выполнении команды мы получим файл с названием CID (идентификатор содержимого файла) и расширением .car
+bafybeifyjj2bjhtxmp235vlfeeiy7sz6rzyx3lervfk3ap2nyn4rggqgei.car
+Содержимое представляет собой двоичную строку, которую может понять только интерпретатор. Тем не менее, мы можем подтвердить размер файла, выполнив простую команду list для проверки файлов, полученных из сети
+ls -hl ./output
+Обратите внимание, что это тот же размер файла, что и исходный файл basic.car, который мы загрузили в сеть. Также можно проверить файлы спомощью утилиты cmp
+cmp basic.car bafybeifyjj2bjhtxmp235vlfeeiy7sz6rzyx3lervfk3ap2nyn4rggqgei.car
